5e5ca2d6f524e1a8da082dc0914c41a17e37d9d3
TOTON Lionel <totonlionel@gmail.com>
2023-12-05 06:58:26 +0100

    KALDOR

--- public/java.js
+++ public/java.js
@@ -61,11 +61,105 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    const pib = document.getElementById('pib');
-    const btc = document.getElementById('btc');
-    const chmg = document.getElementById('chomage');
-    const infl = document.getElementById('inflation');
-    const btnAffichage = document.getElementById('');
+    const btnAnnee2000 = document.getElementById('2000');
+    const btnAnnee2005 = document.getElementById('2005');
+    const btnAnnee2010 = document.getElementById('2010');
+    const btnAnnee2015 = document.getElementById('2015');
+    const btnAnnee2020 = document.getElementById('2020');
+
+    const btnbenin = document.getElementById('btnBenin');
+    const choixAnnee = document.getElementById('choixAnnee')
+    btnbenin.addEventListener('click', function () {
+        choixAnnee.style.display = 'flex';
+    })
+
+    const data2000 = [
+        {
+            type: 'scatterpolar',
+            r: [parseFloat(btc.value), parseFloat(pib.value), parseFloat(chmg.value), parseFloat(infl.value), parseFloat(btc.value)],
+            theta: ['%BTC', '%PIB', '%chomage', '%INFL', '%BTC'],
+            fill: 'toself'
+        },
+
+        {
+            type: 'scatterpolar',
+            r: [5, 10, 10, 10, 5],
+            theta: ['%BTC', '%PIB', '%chomage', '%INFL', '%BTC'],
+            fill: 'toself'
+        }
+
+    ];
+
+    const data2005 = [
+        {
+            type: 'scatterpolar',
+            r: [parseFloat(btc.value), parseFloat(pib.value), parseFloat(chmg.value), parseFloat(infl.value), parseFloat(btc.value)],
+            theta: ['%BTC', '%PIB', '%chomage', '%INFL', '%BTC'],
+            fill: 'toself'
+        },
+
+        {
+            type: 'scatterpolar',
+            r: [5, 10, 10, 10, 5],
+            theta: ['%BTC', '%PIB', '%chomage', '%INFL', '%BTC'],
+            fill: 'toself'
+        }
+
+    ];
+
+    const data2010 = [
+        {
+            type: 'scatterpolar',
+            r: [parseFloat(btc.value), parseFloat(pib.value), parseFloat(chmg.value), parseFloat(infl.value), parseFloat(btc.value)],
+            theta: ['%BTC', '%PIB', '%chomage', '%INFL', '%BTC'],
+            fill: 'toself'
+        },
+
+        {
+            type: 'scatterpolar',
+            r: [5, 10, 10, 10, 5],
+            theta: ['%BTC', '%PIB', '%chomage', '%INFL', '%BTC'],
+            fill: 'toself'
+        }
+
+    ];
+
+    const data2015 = [
+        {
+            type: 'scatterpolar',
+            r: [parseFloat(btc.value), parseFloat(pib.value), parseFloat(chmg.value), parseFloat(infl.value), parseFloat(btc.value)],
+            theta: ['%BTC', '%PIB', '%chomage', '%INFL', '%BTC'],
+            fill: 'toself'
+        },
+
+        {
+            type: 'scatterpolar',
+            r: [5, 10, 10, 10, 5],
+            theta: ['%BTC', '%PIB', '%chomage', '%INFL', '%BTC'],
+            fill: 'toself'
+        }
+
+    ];
+
+    const data2020 = [
+        {
+            type: 'scatterpolar',
+            r: [parseFloat(btc.value), parseFloat(pib.value), parseFloat(chmg.value), parseFloat(infl.value), parseFloat(btc.value)],
+            theta: ['%BTC', '%PIB', '%chomage', '%INFL', '%BTC'],
+            fill: 'toself'
+        },
+
+        {
+            type: 'scatterpolar',
+            r: [5, 10, 10, 10, 5],
+            theta: ['%BTC', '%PIB', '%chomage', '%INFL', '%BTC'],
+            fill: 'toself'
+        }
+
+    ];
+
+
+
 
     btnAffichageBenin.addEventListener('click', function () {
         const data = [
@@ -102,6 +196,4 @@ document.addEventListener("DOMContentLoaded", function () {
         Plotly.newPlot("myDiv", data, layout);
     });
 });
-
-
 
